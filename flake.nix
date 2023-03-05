11f85abac05d78b8f352dfaa88774974003c2318
Thomas Stachl <i@t5.st>
2023-03-05 11:24:48 -0500

    initial commit with half baked flake

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "more modular configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    darwin.url = "github:lnl7/nix-darwin/master";
+  };
+
+  outputs = { self, nixpkgs, darwin, ...}@inputs:
+  let
+    inherit (self) outputs;
+    # supportedSystems = [ "aarch64-linux" "aarch64-darwin" ];
+    # forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  in rec {
+    # nixosModules = import ./modules/nixos;
+    # darwinModules = import ./modules/darwin;
+    # homeModules = import ./modules/home;
+
+    # overlays = import ./overlays;
+
+    nixosConfiguration = {
+      # VPS in Chile
+      thor = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        specialArgs = { inherit inputs; inherit outputs; };
+        modules = [
+          ./hosts/thor
+          ./services/git
+          ./users/thomas
+        ];
+      };
+    };
+
+    darwinConfigurations = {
+      # Macbook Air M2
+      meili = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        specialArgs = { inherit inputs; inherit outputs; };
+        modules = [ ./hosts/meili ./users/thomas ];
+      };
+    };
+  };
+}
