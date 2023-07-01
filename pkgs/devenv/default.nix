8b25a15ca77028aeb0caae2ad7d30b6f9bd0c0e2
Thomas Stachl <i@t5.st>
2023-07-01 10:18:54 -0700

    some more modifications to the docker image

--- pkgs/devenv/default.nix
+++ pkgs/devenv/default.nix
@@ -1,9 +1,29 @@
 { pkgs, ... }:
-pkgs.dockerTools.buildLayeredImage {
+pkgs.dockerTools.buildImageWithNixDb {
   name = "devenv";
   tag = "1.0";
+  copyToRoot = pkgs.buildEnv {
+    name = "image-root";
+    pathsToLink = [ "/bin" ];
+    paths = [
+      # nix-store uses cat program to display results as specified by
+      # the image env variable NIX_PAGER.
+      pkgs.home-manager
+      pkgs.nix
+      pkgs.neovim
+    ];
+  };
+
+  extraCommands = ''
+    #!${pkgs.runtimeShell}
+    ${pkgs.home-manager}/bin/home-manager \
+      --extra-experimental-features "nix-command flakes" \
+      switch \
+        --flake github:tstachl/z#thomas
+  '';
 
   config = {
-    Cmd = [ "${pkgs.hello}/bin/hello" ];
+    Env = [ "USER=nobody" ];
+    Cmd = [ "${pkgs.neovim}/bin/nvim" "/workspace" ];
   };
 }
