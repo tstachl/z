fe6fc04997f0b7b83f1242474d015fe5e86896ed
Thomas Stachl <i@t5.st>
2023-07-22 14:53:11 +0200

    need to figure out a way to import the darwin/nixos module based on platform

--- machines/common/global/home-manager.nix
+++ machines/common/global/home-manager.nix
@@ -1,5 +1,14 @@
-{ inputs, outputs, ... }:
+{ pkgs, inputs, outputs, ... }:
+let
+  # TODO: figure this thing out
+  home-manager-module = with pkgs.stdenv;
+    if isDarwin
+      then inputs.home-manager.darwinModules.home-manager
+      else inputs.home-manager.nixosModules.home-manager;
+in
 {
+  # imports = [ home-manager-module ];
+
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
