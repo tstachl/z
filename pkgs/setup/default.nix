04807b2b3172e9db25f8e358a45e9fca0410b099
Thomas Stachl <i@t5.st>
2023-07-24 10:30:39 -0700

    changes to runtime packages

--- pkgs/setup/default.nix
+++ pkgs/setup/default.nix
@@ -1,6 +1,6 @@
 { writeShellApplication, pkgs }:
 writeShellApplication {
   name = "setup";
-  runtimeInputs = [ pkgs.cryptsetup pkgs.zfs pkgs.parted pkgs.util-linux ];
+  runtimeInputs = [ pkgs.gptfdisk pkgs.cryptsetup pkgs.zfs pkgs.util-linux ];
   text = builtins.readFile ./setup.sh;
 }
