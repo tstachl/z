84cd5151b7357b56388707a655658610317ef641
Thomas Stachl <i@t5.st>
2023-07-30 11:39:23 -0700

    moving zerotier packages around

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,7 +4,7 @@
   devenv = pkgs.callPackage ./devenv {};
   setup = pkgs.callPackage ./setup {};
   geekworm-xscript = pkgs.callPackage ./geekworm-xscript.nix {};
-  zeronsd = pkgs.callPackage ./zeronsd.nix {};
-  zerotier-systemd-manager = pkgs.callPackage ./zerotier-systemd-manager.nix {};
+  zeronsd = pkgs.callPackage ./zerotier/zeronsd.nix {};
+  zerotier-systemd-manager = pkgs.callPackage ./zerotier/zerotier-systemd-manager.nix {};
   # zerotierone = pkgs.callPackage ./zerotierone { };
 }
