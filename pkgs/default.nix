e114e45cb7bb17865633572ee826b448e5adab5d
Thomas Stachl <i@t5.st>
2023-08-28 06:07:30 -0700

    new packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,7 +3,10 @@
   caddy = pkgs.callPackage ./caddy.nix {};
   devenv = pkgs.callPackage ./devenv {};
   geekworm-xscript = pkgs.callPackage ./geekworm-xscript.nix {};
+  # nextdns = pkgs.callPackage ./nextdns.nix {};
   setup = pkgs.callPackage ./setup {};
+  tmux-cht = pkgs.callPackage ./tmux-cht.nix {};
+  hardtime-nvim = pkgs.callPackage ./hardtime-nvim.nix {};
   zeronsd = pkgs.callPackage ./zerotier/zeronsd.nix {};
   zerotier-systemd-manager = pkgs.callPackage ./zerotier/zerotier-systemd-manager.nix {};
   # zerotierone = pkgs.callPackage ./zerotierone { };
