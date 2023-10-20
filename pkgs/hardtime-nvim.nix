4d05fdc57cfcc44452e3e648a8e5ae33f6031e6d
Thomas Stachl <i@t5.st>
2023-10-20 07:54:34 -0700

    adding hardtime vim plugin to packages

--- pkgs/hardtime-nvim.nix
+++ pkgs/hardtime-nvim.nix
@@ -1,12 +1,12 @@
-{ buildVimPluginFrom2Nix, fetchFromGitHub }:
-buildVimPluginFrom2Nix {
+{ vimUtils, fetchFromGitHub }:
+vimUtils.buildVimPluginFrom2Nix {
   pname = "hardtime.nvim";
   version = "2023-08-21";
   src = fetchFromGitHub {
     owner = "m4xshen";
     repo = "hardtime.nvim";
     rev = "778ab462991aa51681e13dea9f4bbe39c336fd73";
-    sha256 = "";
+    sha256 = "sha256-zT1PGeSMpYmErnWESZcRff6QfcMCjptnGYJw/tH9wm8=";
   };
   meta.homepage = "https://github.com/m4xshen/hardtime.nvim";
 }
