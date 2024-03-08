7aee368bd2407adaaa2661da577aa4ca89c46f7b
Thomas Stachl <i@t5.st>
2024-03-08 09:51:30 +0100

    fixed: added some aliases for git

--- users/thomas/cli/shell-aliases.nix
+++ users/thomas/cli/shell-aliases.nix
@@ -1,7 +1,13 @@
 {
-  g = "git";
   "..." = "cd ../..";
   "...." = "cd ../../..";
+
+  # NixOS
   snrs = "sudo nixos-rebuild switch --flake .#$(hostname)";
+
+  # Git stuff
+  g = "git";
+  gco = "git checkout ";
+  gcob = "git checkout -b ";
   ghi = "gh issue create --body '' --title ";
 }
