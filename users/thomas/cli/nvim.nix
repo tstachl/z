2f4fe710083431f96c8b22db11c5e8241f057dee
Thomas Stachl <i@t5.st>
2024-07-08 06:39:35 -0700

    some updates to my nvim config

--- users/thomas/cli/nvim.nix
+++ users/thomas/cli/nvim.nix
@@ -1,13 +1,44 @@
 { inputs, config, pkgs, ... }:
 {
-  imports = [ inputs.nixvim.homeManagerModules.nixvim ];
+  imports = [
+    inputs.nixvim.homeManagerModules.nixvim
+  ];
 
   home.packages = with pkgs; [ ripgrep fd ];
 
   programs.nixvim = {
     enable = true;
 
+    colorschemes.kanagawa = {
+      enable = true;
+      settings.theme = "dragon";
+      settings.transparent = true;
+    };
+
     globals.mapleader = " ";
+
+    keymaps = [
+      { key = "<leader>pv"; action = ":Explore"; mode = "n"; }
+
+      { key = "K"; action = ":m '<-2<CR>gv=gv"; mode = "v"; }
+      { key = "J"; action = ":m '>+1<CR>gv=gv"; mode = "v"; }
+
+      { key = "J"; action = "mzJ`z"; mode = "n"; }
+      { key = "<C-d>"; action = "<C-d>zz"; mode = "n"; }
+      { key = "<C-u>"; action = "<C-u>zz"; mode = "n"; }
+      { key = "n"; action = "nzzzv"; mode = "n"; }
+      { key = "N"; action = "Nzzzv"; mode = "n"; }
+
+      {
+        key = "<C-h>";
+        action.__raw = "vim.lsp.buf.signature_help";
+        mode = "i";
+      }
+
+      { key = "<leader>ot"; action = ":ObsidianToday<cr>"; mode = "n"; }
+      { key = "<leader>on"; action = ":ObsidianNew<cr>"; mode = "n"; }
+      { key = "<leader>os"; action = ":ObsidianSearch<cr>"; mode = "n"; }
+    ];
 
     opts = {
       guicursor = "";
@@ -43,131 +74,28 @@
       colorcolumn = "80";
     };
 
-    keymaps = [
-      { key = "<leader>t"; action = ":NvimTreeToggle<CR>"; mode = "n"; }
-
-      { key = "K"; action = ":m '<-2<CR>gv=gv"; mode = "v"; }
-      { key = "J"; action = ":m '>+1<CR>gv=gv"; mode = "v"; }
-
-      { key = "J"; action = "mzJ`z"; mode = "n"; }
-      { key = "<C-d>"; action = "<C-d>zz"; mode = "n"; }
-      { key = "<C-u>"; action = "<C-u>zz"; mode = "n"; }
-      { key = "n"; action = "nzzzv"; mode = "n"; }
-      { key = "N"; action = "Nzzzv"; mode = "n"; }
-
-      { key = "<leader>p"; action = "[[\"_dP]]"; mode = "v"; }
-
-      { key = "<leader>y"; action = "[[\"+y]]"; }
-      { key = "<leader>Y"; action = "[[\"+Y]]"; mode = "n"; }
-      { key = "<leader>d"; action = "[[\"_d]]"; }
-
-      { key = "<C-c>"; action = "<Esc>"; mode = "i"; }
-
-      { key = "Q"; action = "<nop>"; mode = "n"; }
-
-      { key = "<C-k>"; action = "<cmd>cnext<CR>zz"; mode = "n"; }
-      { key = "<C-j>"; action = "<cmd>cprev<CR>zz"; mode = "n"; }
-      { key = "<leader>k"; action = "<cmd>lnext<CR>zz"; mode = "n"; }
-      { key = "<leader>j"; action = "<cmd>lprev<CR>zz"; mode = "n"; }
-
-      {
-        key = "<leader>s";
-        action = "[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])";
-        mode = "n";
-      }
-
-      {
-        key = "<leader>x";
-        action = "<cmd>!chmod +x %<CR>";
-        options.silent = true;
-        mode = "n";
-      }
-
-      { key = "<leader>u"; action = ":UndotreeToggle<CR>"; mode = "n"; }
-      { key = "<leader>gs"; action = ":Git<CR>"; mode = "n"; }
-      {
-        key = "<C-h>";
-        action.__raw = "vim.lsp.buf.signature_help";
-        mode = "i";
-      }
-    ];
-
-    colorschemes.kanagawa = {
-      enable = true;
-      settings.theme = "dragon";
-      settings.transparent = true;
-    };
-
     plugins = {
-      auto-session.enable = true;
-      tmux-navigator.enable = true;
-
-      # NOTE: Going to start out using the mac app first
-      # obsidian = {
-      #   enable = true;
-      #   settings = {
-      #     new_notes_location = "notes_subdir";
-      #     notes_subdir = "~/Documents/Notes/6 - Full Notes";
-      #     workspaces = [
-      #       { name = "personal"; path = "~/Documents/Notes"; }
-      #     ];
-      #   };
-      # };
-
-      telescope = {
-        enable = true;
-        settings.pickers.find_files.follow = true;
-        keymaps = {
-          "<leader>e" = "find_files";
-          "<C-p>" = "git_files";
-          "<leader>ps" = "grep_string";
-          "<leader>lg" = "live_grep";
-          "<leader>vh" = "help_tags";
-        };
-        extensions.fzf-native.enable = true;
-      };
-
-      treesitter = {
-        enable = true;
-        ensureInstalled = "all";
-        indent = true;
-        nixvimInjections = true;
-      };
-      treesitter-context.enable = true;
-
-      nvim-tree = {
-        enable = true;
-        disableNetrw = true;
-        extraOptions = {
-          renderer = {
-            root_folder_label = false;
-            icons = {
-              glyphs = {
-                default = "";
-                symlink = "";
-                git = {
-                  unstaged = "";
-                  staged = "S";
-                  unmerged = "";
-                  renamed = "➜";
-                  deleted = "";
-                  untracked = "U";
-                  ignored = "◌";
-                };
-                folder = {
-                  default = "";
-                  open = "";
-                  empty = "";
-                  empty_open = "";
-                  symlink = "";
-                };
-              };
-            };
-          };
-        };
-      };
-
       comment.enable = true;
+      tmux-navigator.enable = true;
+      fidget.enable = true;
+
+      cmp = {
+        enable = true;
+        settings.mapping = {
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-e>" = "cmp.mapping.close()";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<C-p>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<C-n>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+        };
+      };
+      cmp-buffer.enable = true;
+      cmp-path.enable = true;
+      cmp_luasnip.enable = true;
+      cmp-nvim-lsp.enable = true;
+      cmp-nvim-lua.enable = true;
 
       lsp = {
         enable = true;
@@ -200,9 +128,7 @@
 
           denols = {
             enable = true;
-            rootDir = ''
-              require('lspconfig').util.root_pattern("deno.json")
-            '';
+            rootDir = "require('lspconfig').util.root_pattern('deno.json')";
             extraOptions.init_options = {
               lint = true;
               unstable = true;
@@ -210,16 +136,7 @@
           };
 
           elixirls.enable = true;
-          elixirls.package = pkgs.unstable.elixir-ls;
-
           nil-ls.enable = true;
-
-          rust-analyzer = {
-            enable = true;
-            installCargo = true;
-            installRustc = true;
-          };
-
           tailwindcss.enable = true;
 
           tsserver = {
@@ -227,28 +144,138 @@
             extraOptions.single_file_support = false;
             rootDir = ''
               require('lspconfig').util.root_pattern("tsconfig.json")
-            '';
+              '';
           };
 
           volar = {
             enable = true;
             rootDir = ''
               require('lspconfig').util.root_pattern("vite.config.mts")
-            '';
+              '';
           };
         };
       };
 
-      fidget.enable = true;
-      cmp.enable = true;
-      cmp-buffer.enable = true;
-      cmp-path.enable = true;
-      cmp_luasnip.enable = true;
-      cmp-nvim-lsp.enable = true;
-      cmp-nvim-lua.enable = true;
-      luasnip.enable = true;
+      obsidian = {
+        enable = true;
+        settings = {
+          daily_notes = {
+            folder = "daily";
+            date_format = "%Y-%m-%d";
+            alias_format = "%B %-d, %Y";
+            template = "daily.md";
+          };
 
-      trouble.enable = true;
+          new_notes_location = "notes_subdir";
+
+          note_id_func = ''
+            function(title)
+              -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+              -- In this case a note with the title 'My new note' will be given an ID that looks
+              -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+              local suffix = ""
+              if title ~= nil then
+                -- If title is given, transform it into valid file name.
+                suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+              else
+                -- If title is nil, just add 4 random uppercase letters to the suffix.
+                for _ = 1, 4 do
+                  suffix = suffix .. string.char(math.random(65, 90))
+                end
+              end
+              return tostring(os.date('%Y%m%d%H%M')) .. "-" .. suffix
+            end
+          '';
+
+          open_notes_in = "hsplit";
+
+          templates = {
+            folder = "templates";
+            date_format = "%Y-%m-%d";
+            time_format = "%H:%M";
+            substitutions = {
+              "date:YYYY-MM-DD".__raw = ''
+                function()
+                  return os.date("%Y-%m-%d", os.time())
+                end
+              '';
+
+              "date:MMMM D, YYYY".__raw = ''
+                function()
+                  return os.date("%B %d, %Y", os.time()):gsub(" 0", " ")
+                end
+              '';
+
+              yesterday.__raw = ''
+                function()
+                  return os.date("%Y-%m-%d", os.time() - 86400)
+                end
+              '';
+
+              tomorrow.__raw = ''
+                function()
+                  return os.date("%Y-%m-%d", os.time() + 86400)
+                end
+              '';
+            };
+          };
+
+          workspaces = [{
+            name = "Notes";
+            path = "/Users/thomas/Obsidian/Notes";
+            overrides = {
+              notes_subdir = "notes";
+            };
+          }];
+        };
+      };
+
+      telescope = {
+        enable = true;
+        settings.pickers.find_files.follow = true;
+        keymaps = {
+          "<leader>e" = "find_files";
+          "<C-p>" = "git_files";
+          "<leader>gs" = "grep_string";
+          "<leader>lg" = "live_grep";
+          "<leader>vh" = "help_tags";
+        };
+        extensions.fzf-native.enable = true;
+      };
+
+      treesitter = {
+        enable = true;
+        ensureInstalled = "all";
+        indent = true;
+        nixvimInjections = true;
+      };
+      treesitter-context.enable = true;
+
+      zen-mode = {
+        enable = true;
+        settings = {
+          plugins.alacritty.enabled = true;
+          plugins.tmux.enabled = true;
+          window.width = 0.6;
+        };
+      };
     };
+
+    extraPlugins = with pkgs.vimPlugins; [ wrapping-nvim ];
+    extraConfigLua = ''
+      require("wrapping").setup({
+        auto_set_mode_filetype_allowlist = {
+          "asciidoc",
+          "gitcommit",
+          "latex",
+          "mail",
+          "markdown",
+          "rst",
+          "tex",
+          "text",
+        },
+        softener = { markdown = true },
+      })
+    '';
   };
 }
