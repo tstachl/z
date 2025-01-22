843f33540ab4d64f045c505d9030f9d862952ec4
Thomas Stachl <i@t5.st>
2025-01-22 16:03:05 -0500

    adding modgud

---
+++ machines/modgud/default.nix
@@ -0,0 +1,28 @@
+{
+  imports = [
+    ./hardware.nix
+
+    ../common/global
+    ../common/optional/gnupg.nix
+    ../common/optional/locale.nix
+    ../common/optional/fish.nix
+    ../common/optional/nixos.nix
+    ../common/optional/openssh.nix
+
+    ../common/users/thomas
+    ../common/users/thomas/authorized_keys.nix
+    ../common/users/thomas/groups.nix
+    ../common/users/thomas/nixos.nix
+  ];
+
+  boot.loader.grub.enable = true;
+
+  security.sudo.extraConfig = ''
+    # rollback results in sudo lectures after each reboot
+    Defaults lecture = never
+  '';
+
+  networking.hostName = "modgud";
+  time.timeZone = "Europe/Amsterdam";
+  system.stateVersion = "25.05";
+}
